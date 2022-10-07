f8e03625897546bf089dfea2489a713b0a7f1762
Jona Löffler <jona.loeffler@online.de>
2022-10-07 08:57:38 +0200

    Updates

--- .config/nvim/after/plugin/remaps/nerdtree.lua
+++ .config/nvim/after/plugin/remaps/nerdtree.lua
@@ -1,6 +1,6 @@
 vim.g.NERDTreeShowHidden = 1
 vim.g.NERDTreeWinSize = 30
-vim.g.NERDTreeMinimalUI=1
+vim.g.NERDTreeMinimalUI = 1
 vim.keymap.set("n", "<leader>e", ":NERDTreeFocus<CR>")
 vim.keymap.set("n", "<leader>n", ":NERDTree<CR>")
 vim.keymap.set("n", "<leader>nr", ":NERDTreeRefreshRoot<CR>")
