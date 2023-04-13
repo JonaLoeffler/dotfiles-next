fa4f58e517212a70a729652e55ca16195ae0c23f
Jona Löffler <jona.loeffler@itemis.com>
2023-04-13 11:24:44 +0200

    Update

--- .config/nvim/after/plugin/orgmode.lua
+++ .config/nvim/after/plugin/orgmode.lua
@@ -2,6 +2,18 @@
 require('orgmode').setup_ts_grammar()
 
 require('orgmode').setup({
-  org_agenda_files = {'~/Documents/org/*'},
-  org_default_notes_file = '~/Documents/org/refile.org',
+    org_agenda_files = {'~/Documents/org/*'},
+    org_default_notes_file = '~/Documents/org/refile.org',
+    org_capture_templates = {
+        w = {
+            description = "Day Work Log",
+            template = "*** %t 8h Homeoffice\n  - %?",
+            target = "~/Documents/org/work/worklog.org",
+        },
+        j = {
+            description = "Journal",
+            template = "** %t\n - %?",
+            target = "~/Documents/org/journal.org",
+        }
+    }
 })
